17894aab50a1426e92f601c01723d361c9f61ef0
nhatminh237 <minhnhatnguyen237@gmail.com>
2021-01-05 10:09:40 +0700

    Update BUS

--- DoAnQLKS/QuanLyKhachSan/BusinessLayer/HoaDonThanhToanBUS.cs
+++ DoAnQLKS/QuanLyKhachSan/BusinessLayer/HoaDonThanhToanBUS.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QuanLyKhachSan.DataAccess;
+
+namespace QuanLyKhachSan.BusinessLayer
+{
+    public class HoaDonThanhToanBUS
+    {
+        public void insertHoaDon(string mahd, string tenphong, string ngaythanhtoan, int songaythue, double dongia, double phuthu, double thanhtien, string phieuthue)
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            dao.insertHoaDon(mahd, tenphong, ngaythanhtoan, songaythue, dongia, phuthu, thanhtien, phieuthue);
+        }
+        public DataTable selectAllHoaDon()
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            return dao.selectAllHoaDon();
+        }
+        public DataTable KiemTraThanhToan()
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            return dao.Kiemtrathanhtoan();
+        }
+
+        public DataTable SelectNam()
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            return dao.SelectNam();
+        }
+        public DataTable selectDoanhThuTheoLoaiPhong(int thang, int nam)
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            return dao.selectDoanhThuTheoLoaiPhong(thang, nam);
+        }
+        public DataTable selectMatDoSuDungPhong(int thang, int nam)
+        {
+            HoaDonThanhToanDAO dao = new HoaDonThanhToanDAO();
+            return dao.selectMatDoSuDungPhong(thang, nam);
+        }
+    }
+}
