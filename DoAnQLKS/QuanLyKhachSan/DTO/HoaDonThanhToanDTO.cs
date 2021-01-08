b484f5e6fa2e2fe0e78768a162ad879e4352d7b2
Phạm Thị Hằng <50732089+HangNho@users.noreply.github.com>
2021-01-09 00:04:47 +0700

    Update HoaDonThanhToanDTO.cs

--- DoAnQLKS/QuanLyKhachSan/DTO/HoaDonThanhToanDTO.cs
+++ DoAnQLKS/QuanLyKhachSan/DTO/HoaDonThanhToanDTO.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace QuanLyKhachSan.DTO
 {
-    class HoaDonThanhToanDTO
+    public class HoaDonThanhToanDTO
     {
+        public string mahoadon;
+        public string phong;
+        public string ngaythanhtoan;
+        public double songaythue;
+        public double dongia;
+        public double phuthu;
+        public double thanhtien;
+        public string phieuthue;
     }
 }
